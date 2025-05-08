983cecf5ffb71c419e20f74ddcdd23f567b6d288
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-08 17:42:15 +0300

    Assignment 1 settled

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,14 @@
 import s from './DecksList.module.css'
+import { useEffect } from 'react'
+import { useAppDispatch } from '../../../app/store.ts'
+import { decksApi } from '../decks-api.ts'
 
 export const DecksList = () => {
+  useEffect(() => {
+    decksApi.fetchDecks().then((res) => {
+      console.log(res.data.items)
+    })
+  }, [])
+
   return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,39 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+export const decksApi = {
+  fetchDecks() {
+    return instance.get<FetchDecksResponse>('/v2/decks')
+  },
+}
+
+type FetchDecksResponse = {
+  items: Deck[]
+  pagination: Pagination
+}
+
+type Deck = {
+  author: Author
+  id: string
+  userId: string
+  name: string
+  isFavorite: boolean
+  isPrivate: true
+  cover: string
+  created: string
+  updated: string
+  cardsCount: number
+}
+
+type Author = {
+  id: string
+  name: string
+}
+
+type Pagination = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
