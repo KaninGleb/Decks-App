42ab793b0b91566b3016d0e6cf8a3da0c79674b8
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-08 18:43:12 +0300

    Assignment 3 - DecksList logic moved to custom hook useFetchDecksList

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,24 +1,15 @@
-import { useEffect } from 'react'
-import { useAppSelector, useAppDispatch } from '../../../app/store.ts'
-import { decksApi } from '../decks-api.ts'
-import { selectDecks } from '../decks-selectors.ts'
-import { setDecksAC } from '../decks-reducer.ts'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
 import s from './DecksList.module.css'
+import { useFetchDecksList } from './useFetchDecks.ts'
 
 export const DecksList = () => {
-  const dispatch = useAppDispatch()
-  const decks = useAppSelector(selectDecks)
-
-  useEffect(() => {
-    decksApi.fetchDecks().then((res) => {
-      dispatch(setDecksAC(res.data.items))
-    })
-  }, [])
+  const { decks } = useFetchDecksList()
 
   return (
     <ul className={s.list}>
-      {decks?.map((deck) => <DeckItem key={deck.id} deck={deck} />)}
+      {decks?.map((deck) => {
+        return <DeckItem key={deck.id} deck={deck} />
+      })}
     </ul>
   )
 }
