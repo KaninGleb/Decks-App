cb9cfda23d5e12fab0900bf2826f4e39b4dbf479
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-08 18:27:05 +0300

    Assignment 2 - DecksList supplemented

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,14 +1,24 @@
-import s from './DecksList.module.css'
 import { useEffect } from 'react'
-import { useAppDispatch } from '../../../app/store.ts'
+import { useAppSelector, useAppDispatch } from '../../../app/store.ts'
 import { decksApi } from '../decks-api.ts'
+import { selectDecks } from '../decks-selectors.ts'
+import { setDecksAC } from '../decks-reducer.ts'
+import { DeckItem } from './DeckItem/DeckItem.tsx'
+import s from './DecksList.module.css'
 
 export const DecksList = () => {
+  const dispatch = useAppDispatch()
+  const decks = useAppSelector(selectDecks)
+
   useEffect(() => {
     decksApi.fetchDecks().then((res) => {
-      console.log(res.data.items)
+      dispatch(setDecksAC(res.data.items))
     })
   }, [])
 
-  return <ul className={s.list}></ul>
+  return (
+    <ul className={s.list}>
+      {decks?.map((deck) => <DeckItem key={deck.id} deck={deck} />)}
+    </ul>
+  )
 }
