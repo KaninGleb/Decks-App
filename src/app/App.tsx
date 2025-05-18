b8589a96492489a5eb031708d34fb72e7661b8a6
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-18 22:26:17 +0300

    Assignment 6 - footer added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,13 +6,17 @@ import { Decks } from '../features/decks/Decks.tsx'
 import { GlobalError } from './GlobalError/GlobalError.tsx'
 
 export const App = () => {
-const status = useAppSelector(selectAppStatus)
+  const status = useAppSelector(selectAppStatus)
 
   return (
     <div>
       {status === 'loading' && <LinearLoader />}
       <Decks />
       <GlobalError />
+      <footer>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aperiam commodi cumque dolorum earum
+        et, harum illum laudantium maiores minima obcaecati pariatur quam, tempore voluptates? Itaque nam obcaecati quo?
+      </footer>
     </div>
   )
 }
