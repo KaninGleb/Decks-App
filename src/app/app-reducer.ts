3b97a4a586e0f89bcccf51a4f1664ceac83451cd
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-18 21:27:27 +0300

    Assignment 4.2 - SetAppErrorAC added

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -5,15 +5,26 @@ const initialState = {
   error: null as string | null,
 }
 
-export const SetAppStatusAC = (status: RequestStatusType) => ({
-  type: 'APP/SET-STATUS',
-  status,
-}) as const
+export const SetAppStatusAC = (status: RequestStatusType) =>
+  ({
+    type: 'APP/SET-STATUS',
+    status,
+  }) as const
+
+export const SetAppErrorAC = (error: string | null) =>
+  ({
+    type: 'APP/SET-ERROR',
+    error,
+  }) as const
 
 export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
   switch (action.type) {
     case 'APP/SET-STATUS': {
       return { ...state, status: action.status }
+    }
+
+    case 'APP/SET-ERROR': {
+      return { ...state, error: action.error }
     }
 
     default:
@@ -23,6 +34,7 @@ export const appReducer = (state: AppStateType = initialState, action: ActionsTy
 
 type AppStateType = typeof initialState
 
-type ActionsType = UpdateLoadingStatusAT
+type ActionsType = UpdateLoadingStatusAT | SetAppErrorAT
 
 type UpdateLoadingStatusAT = ReturnType<typeof SetAppStatusAC>
+type SetAppErrorAT = ReturnType<typeof SetAppErrorAC>
